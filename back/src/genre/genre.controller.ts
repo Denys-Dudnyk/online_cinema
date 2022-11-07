c3b2a6b957704ae6bfcd72bba1ba1101c23f2d9b
Denys-Dudnyk <83369962+Denys-Dudnyk@users.noreply.github.com>
2022-11-07 22:33:21 +0100

    Back-End || Genre (Update,Del, Create + Slug)

---
+++ back/src/genre/genre.controller.ts
@@ -0,0 +1,70 @@
+import {
+	Body,
+	Controller,
+	Delete,
+	Get,
+	HttpCode,
+	Param,
+	Post,
+	Put,
+	Query,
+	UsePipes,
+	ValidationPipe,
+} from '@nestjs/common'
+import { Auth } from 'src/auth/decorators/auth.decorator'
+import { IdValidationPipe } from 'src/pipes/id.validation.pipe'
+import { GenreService } from './genre.service'
+import { CreateGenreDto } from './dto/create-genre.dto'
+
+@Controller('genres')
+export class GenreController {
+	constructor(private readonly genreService: GenreService) {}
+
+	@Get('by-slug/:slug')
+	async bySlug(@Param('slug') slug: string) {
+		return this.genreService.bySlug(slug)
+	}
+
+	@Get('/collections')
+	async getCollections() {
+		return this.genreService.getCollections()
+	}
+
+	@Get()
+	async getAll(@Query('searchTerm') searchTerm?: string) {
+		return this.genreService.getAll(searchTerm)
+	}
+
+	/* Admin */
+	@Get(':id')
+	@Auth('admin')
+	async get(@Param('id', IdValidationPipe) id: string) {
+		return this.genreService.byId(id)
+	}
+
+	@UsePipes(new ValidationPipe())
+	@Post()
+	@HttpCode(200)
+	@Auth('admin')
+	async create() {
+		return this.genreService.create()
+	}
+
+	@UsePipes(new ValidationPipe())
+	@Put(':id')
+	@HttpCode(200)
+	@Auth('admin')
+	async update(
+		@Param('id', IdValidationPipe) id: string,
+		@Body() dto: CreateGenreDto
+	) {
+		return this.genreService.update(id, dto)
+	}
+
+	@Delete(':id')
+	@HttpCode(200)
+	@Auth('admin')
+	async delete(@Param('id', IdValidationPipe) id: string) {
+		return this.genreService.delete(id)
+	}
+}
---
+++ back/src/genre/genre.service.ts
@@ -0,0 +1,88 @@
+import { Injectable, NotFoundException } from '@nestjs/common'
+import { ModelType } from '@typegoose/typegoose/lib/types'
+import { genSalt } from 'bcryptjs'
+import { InjectModel } from 'nestjs-typegoose'
+import { CreateGenreDto } from './dto/create-genre.dto'
+import { GenreModel } from './genre.model'
+
+@Injectable()
+export class GenreService {
+	constructor(
+		@InjectModel(GenreModel) private readonly GenreModel: ModelType<GenreModel>
+	) {}
+
+	async bySlug(slug: string) {
+		return this.GenreModel.findOne({ slug }).exec()
+	}
+
+	async getAll(searchTerm?: string) {
+		let options = {}
+
+		if (searchTerm)
+			options = {
+				$or: [
+					{
+						name: new RegExp(searchTerm, 'i'),
+					},
+					{
+						slug: new RegExp(searchTerm, 'i'),
+					},
+					{
+						description: new RegExp(searchTerm, 'i'),
+					},
+				],
+			}
+		return this.GenreModel.find(options)
+			.select('-updatedAt -__v')
+			.sort({
+				createdAt: 'desc',
+			})
+			.exec()
+	}
+
+	async getCollections() {
+		const genres = await this.getAll()
+		const collections = genres
+		/*Need to write*/
+		return collections
+	}
+
+	/* Admin Place */
+
+	async byId(_id: string) {
+		const genre = await this.GenreModel.findById(_id)
+
+		if (!genre) throw new NotFoundException('Genre not found')
+
+		return genre
+	}
+
+	async create() {
+		const defaultValue: CreateGenreDto = {
+			name: '',
+			slug: '',
+			description: '',
+			icon: '',
+		}
+		const genre = await this.GenreModel.create(defaultValue)
+		return genre._id
+	}
+
+	async update(_id: string, dto: CreateGenreDto) {
+		const updateDoc = await this.GenreModel.findByIdAndUpdate(_id, dto, {
+			new: true,
+		}).exec()
+
+		if (!updateDoc) throw new NotFoundException('Genre not found')
+
+		return updateDoc
+	}
+
+	async delete(id: string) {
+		const deleteDoc = await this.GenreModel.findByIdAndDelete(id).exec()
+
+		if (!deleteDoc) throw new NotFoundException('Genre not found')
+
+		return deleteDoc
+	}
+}
