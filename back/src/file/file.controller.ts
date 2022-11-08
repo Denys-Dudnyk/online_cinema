26485f68843b5de3126ddbc7d07b4cc2e31d4d48
Denys-Dudnyk <83369962+Denys-Dudnyk@users.noreply.github.com>
2022-11-08 22:15:57 +0100

    Back-End || File upload (Multer)

---
+++ back/src/file/file.controller.ts
@@ -0,0 +1,27 @@
+import {
+	Controller,
+	Post,
+	HttpCode,
+	UseInterceptors,
+	UploadedFile,
+	Query,
+} from '@nestjs/common'
+import { FileInterceptor } from '@nestjs/platform-express'
+import { Auth } from 'src/auth/decorators/auth.decorator'
+import { FileService } from './file.service'
+
+@Controller('files')
+export class FileController {
+	constructor(private readonly fileService: FileService) {}
+
+	@Post()
+	@HttpCode(200)
+	@Auth('admin')
+	@UseInterceptors(FileInterceptor('image'))
+	async uploadFile(
+		@UploadedFile() file: Express.Multer.File,
+		@Query('folder') folder?: string
+	) {
+		return this.fileService.saveFiles([file], folder)
+	}
+}
