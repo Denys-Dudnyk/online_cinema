e67cd2bbc5a7fe67ea6a1b2ec03a46cb84c654ae
Denys-Dudnyk <83369962+Denys-Dudnyk@users.noreply.github.com>
2022-11-09 22:28:22 +0100

    Back-End || Actors (Update ,Create ,Delete)

--- back/src/genre/genre.service.ts
+++ back/src/genre/genre.service.ts
@@ -12,7 +12,9 @@ export class GenreService {
 	) {}
 
 	async bySlug(slug: string) {
-		return this.GenreModel.findOne({ slug }).exec()
+		const doc = await this.GenreModel.findOne({ slug }).exec()
+		if (!doc) throw new NotFoundException('Genre not found')
+		return doc
 	}
 
 	async getAll(searchTerm?: string) {
